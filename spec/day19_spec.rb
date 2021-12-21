17cbd9039f6bc8a53507f5f6a3b62c311de54180
José Rodrigues <contact.jrodrigues@gmail.com>
2021-12-21 18:22:37 +0100

    19th day : put right values in test

--- spec/day19_spec.rb
+++ spec/day19_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../day19/day19.rb'
 
 describe Day19 do
-    # it_behaves_like 'aoc_challenge_part1_example', 79
-    # it_behaves_like 'aoc_challenge_part1_real_input', 449
-    # it_behaves_like 'aoc_challenge_part2_example', 3621
-    it_behaves_like 'aoc_challenge_part2_real_input', 0
+    it_behaves_like 'aoc_challenge_part1_example', 79
+    it_behaves_like 'aoc_challenge_part1_real_input', 449
+    it_behaves_like 'aoc_challenge_part2_example', 3621
+    it_behaves_like 'aoc_challenge_part2_real_input', 13128
 end
